55d600192a31c279988e0437c2372ec9b89876ba
fcostasantos <contato.fcosta1@gmail.com>
2023-01-11 20:42:59 -0300

    Teste

--- operadores/challenge_assignment.js
+++ operadores/challenge_assignment.js
@@ -5,6 +5,6 @@ let texto =  'Lista de Aprovados \n=================\n ';
 texto = texto + '\n 1. Ana Silva';
 texto = texto + '\n 2. Pedro Albuquerque';
 texto = texto + '\n 3. Guilherme Pereira';
-texto = texto + '\n 4. Rebeca França';
+texto = texto + '\n 4. Rebeca Soares';
 
 console.log(texto);
