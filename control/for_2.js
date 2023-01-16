27648be72b3a6b9234ad005e098ef932d91c146c
fcostasantos <contato.fcosta1@gmail.com>
2023-01-16 16:25:26 -0300

    more exercises

---
+++ control/for_2.js
@@ -0,0 +1,17 @@
+//for(;;){ //Loop
+
+
+//}
+
+//for(;  true;){
+
+//}
+let controle = 1;
+
+
+for (; controle <= 10;) {
+    console.log(controle);
+    controle++;
+}
+
+console.log('Fim!')
---
+++ control/while_4.js
@@ -0,0 +1,9 @@
+
+let controle = 1;
+
+while (controle <= 10) {
+    console.log(controle);
+    controle++;
+}
+
+//declaração , expressão e incremento -> Qtde determinada usando o while!
