777a6f7770b7e1892250bdc4059cd4865f177d20
fcostasantos <contato.fcosta1@gmail.com>
2023-01-19 16:14:17 -0300

    new file:   Funcoes/no_function.js 	new file:   Funcoes/with_function_1.js 	new file:   control/break_continue.js 	new file:   control/challenge_switch.js

---
+++ Funcoes/no_function.js
@@ -0,0 +1,16 @@
+
+const d1 = 3;
+const m1 = 7;
+const a1 = 2023;
+
+console.log(`Dia: ${d1}`);
+console.log(`Dia: ${m1}`);
+console.log(`Dia: ${a1}`);
+
+const d2 = 22;
+const m2 = 12;
+const a2 = 2023
+
+console.log(`Dia: ${d2}`);
+console.log(`Dia: ${m2}`);
+console.log(`Dia: ${a2}`);
---
+++ Funcoes/with_function_1.js
@@ -0,0 +1,22 @@
+const d1 = 3;
+const m1 = 7;
+const a1 = 2023;
+
+function exibirData1() {
+    console.log(`Dia: ${d1}`);
+    console.log(`Dia: ${m1}`);
+    console.log(`Dia: ${a1}`);
+
+}
+
+const d2 = 22;
+const m2 = 12;
+const a2 = 2023
+
+function exibirData2() {
+    console.log(`Dia: ${d2}`);
+    console.log(`Dia: ${m2}`);
+    console.log(`Dia: ${a2}`);
+}
+
+exibirData1();
